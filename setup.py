d2c985996d4f80a3748e0a04f1f5de0fb098f8e9
LBrinkmann <com@levin-brinkmann.de>
2018-08-29 23:59:20 +0200

    Add setup py.

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+# configure these
+PACKAGE_NAME = 'redi-datascience-utils'
+MODULE_NAME = 'redi'
+VERSION = '0.0.1'
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+            'matplotlib==2.0.0',
+            'seaborn==0.7.1',
+            'numpy',
+            'pandas'
+        ],
+
+    extras_require={
+        'dev': []
+    },
+
+    cmdclass={
+    },
+
+    scripts=[
+    ],
+
+    # run `python3 setup.py prepare_build`
+    # followed by `python3 setup.py bdist`
+    # then you can check the `build` folder to see if your package data is included in
+    # the binary distribution
+    package_data={
+        '': [
+            'resources/*',
+            'config/*'
+        ]
+    }
+)
