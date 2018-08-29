230163db7ab43b5fbd8c3892e67131dd42832ac5
LBrinkmann <com@levin-brinkmann.de>
2018-08-30 00:18:23 +0200

    Update spider plot.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 # configure these
 PACKAGE_NAME = 'redi-datascience-utils'
 MODULE_NAME = 'redi'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 setup(
     name=PACKAGE_NAME,
